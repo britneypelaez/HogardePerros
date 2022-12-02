57a02cf542c9ee307cdcc918ff43e3b85f43fb17
camiloparrado <camilo.parrado@unillanos.edu.co>
2022-12-01 21:26:25 -0500

    vista del login con registro google

--- resources/views/vendor/voyager/login.blade.php
+++ resources/views/vendor/voyager/login.blade.php
@@ -9,38 +9,40 @@
     <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" rel="stylesheet" />
 </head>
 <body>
-    <div class="login-card-container">
+<div class="login-card-container">
         <div class="login-card">
            <div class="login-card-logo">
-            <?php $admin_logo_img = Voyager::setting('admin.icon_image', ''); ?>
-             <img src="{{ Voyager::image($admin_logo_img) }}" alt="Logo">
-           </div>
+             <img src="/IMG/LOGO.png" alt="Logo">
+           </div> 
            <div class="login-card-header">
                 <h1>Ingresa</h1>
                 <div>Por favor inicie sesión para usar la plataforma</div>
            </div>
-           <form action="{{ route('voyager.login') }}" class="login-card-form" method="post">
+           <form action="" class="login-card-form" method="post">
             <div class="form-item">
                 <span class="form-item-icon material-symbols-outlined">email</span>
-                <input type="email" name="email" id="email" value="{{ old('email') }}" placeholder="{{ __('voyager::generic.email') }}" required autofocus>
+                <input type="email" name="email" id="email" placeholder="Ingresa Email" required autofocus>
             </div>
             <div class="form-item">
                 <span class="form-item-icon material-symbols-outlined">lock</span>
-                <input type="password" name="password" id="password" placeholder="{{ __('voyager::generic.password') }}" required>
+                <input type="password" name="password" id="password" placeholder="Ingresa Contraseña" required>
             </div>
-            <div class="form-item-other">
-                <div class="checkbox">
-                    <input type="checkbox" name="recordarcheckbox" id="recordarcheckbox">
-                    <label for="recordarcheckbox">Recordar</label>
-                </div>
-                <a href="#">Olvidé mi contraseña</a>
-            </div>
-            @csrf
             <button type="submit">Ingresar</button>
            </form>
            <div class="login-card-footer">
-            No tienes una cuenta?<a href="{{ route('admin/register') }}">Create una cuenta</a>
+            No tienes una cuenta?<a href="{{ route('password.request') }}">Create una cuenta</a>
            </div>
+           <div class="login-card-social">
+            <div>Inicio de sesión con google</div>
+            <div class="login-card-social-btn">
+                <a href="/login-google">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-google" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                        <path d="M17.788 5.108a9 9 0 1 0 3.212 6.892h-8" />
+                    </svg>
+                </a>
+            </div>
+        </div>
         </div>
     </div>
 </body>
