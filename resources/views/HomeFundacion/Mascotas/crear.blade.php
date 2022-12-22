d4ee07758dd6b78a1f2a1c67f9ca64aebf4ad617
JhonRoa <jhon.roa019@gmail.com>
2022-12-22 00:55:56 -0500

    ModalAdopcionFundacion

--- resources/views/HomeFundacion/Mascotas/crear.blade.php
+++ resources/views/HomeFundacion/Mascotas/crear.blade.php
@@ -1,120 +1,117 @@
-@extends('layouts.HomeFundacion.app')
+@if($errors->any())
+<div class="alert alert-dark alert-dismissible fade show" role="alert">
+    <strong>¡Revise los campos!</strong>
+    @foreach($errors->all() as $error)
+    <span class="badge badge-danger">{{$error}}</span>
+    @endforeach
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+    </button>
+</div>
+@endif
 
-@section('content')
-    <section class="section">
-        <div class="section-header">
-            <h3 class="page__heading">Nuevo Animal</h3>
-        </div>
-        <div class="section-body">
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="card">
-                        <div class="card-body">
+<div class="contenedor-modal" id="modalsCreate">
+    <div class="moda">
+            <form action="{{ route('Mascotas.store') }}" method="POST" enctype="multipart/form-data">
+                @csrf
+                <div class="informacion">
 
-                        @if($errors->any())
-                            <div class="alert alert-dark alert-dismissible fade show" role="alert">
-                            <strong>¡Revise los campos!</strong>
-                                @foreach($errors->all() as $error)
-                                    <span class="badge badge-danger">{{$error}}</span>
-                                @endforeach
-                                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                                </button>
-                            </div>
-                            @endif
-                            
-                            <form action="{{ route('Mascotas.store') }}" method="POST" enctype="multipart/form-data">
-                            @csrf
-                            <div class="row">
-                                <div class="col-xs-12 col-sm-12 col-md-12">
-                                    <div class="form-group">
-                                        <label for="nombre_mascota">Nombre de la mascota</label>
-                                        <input type="text" name="nombre_mascota" class="form-control" required value="{{ old('nombre_mascota') }}">
-                                    </div>
-                                </div>
 
-                                <div class="col-xs-12 col-sm-12 col-md-12">
-                                    <div class="form-group">
-                                        <label for="descripcion">Descripcion</label>
-                                        <input type="text" name="descripcion" class="form-control" required value="{{ old('descripcion') }}">
-                                    </div>
-                                </div>
+                <h3 class="page__heading">Nuevo Animal</h3>
+                <div class="actualizar1">
+                <img src="" alt=""> <!-- Tratar de implementar la img q se selecciona ¡-->
 
-                                <div class="col-xs-12 col-sm-12 col-md-12">
-                                    <div class="form-group">
-                                        <label for="raza">Raza</label>
-                                        <select name="raza" id="raza" class="form-control">
-                                            @foreach($razas as $raza)
-                                            <option value="{{ $raza['raza'] }}">{{ $raza['descripcion'] }}</option>
-                                            @endforeach
-                                        </select>
-                                    </div>
-                                </div>
-
-                                <div class="col-xs-12 col-sm-12 col-md-12">
-                                    <div class="form-group">
-                                        <label for="color">Color</label>
-                                        <select name="color" id="color" class="form-control">
-                                            @foreach($colores as $color)
-                                            <option value="{{ $color['color'] }}">{{ $color['descripcion'] }}</option>
-                                            @endforeach
-                                        </select>
-                                    </div>
-                                </div>
-
-                                <div class="col-xs-12 col-sm-12 col-md-12">
-                                    <div class="form-group">
-                                        <label for="tamanio">Tamaño</label>
-                                        <select name="tamanio" id="tamanio" class="form-control">
-                                            @for ($i = 20; $i <=110; $i = $i + 5)
-                                            <option value="{{ $i }}">{{ $i }} cm</option>
-                                            @endfor
-                                        </select>
-                                    </div>
-                                </div>
-
-                                <div class="col-xs-12 col-sm-12 col-md-12">
-                                    <div class="form-group">
-                                        <label for="especie">Especie</label>
-                                        <select name="especie" id="especie" class="form-control">
-                                            @foreach($especies as $especie)
-                                            <option value="{{ $especie['especie'] }}">{{ $especie['descripcion'] }}</option>
-                                            @endforeach
-                                        </select>
-                                    </div>
-                                </div>
-
-                                <div class="col-xs-12 col-sm-12 col-md-12">
-                                    <div class="form-group">
-                                        <label for="edad">Edad</label>
-                                        <select name="edad" id="edad" class="form-control">
-                                            @for ($i = 0; $i <=14; $i++)
-                                            <option value="{{ $i }}">{{ $i }} años</option>
-                                            @endfor
-                                        </select>
-                                    </div>
-                                </div>
-
-                                <div class="col-xs-12 col-sm-12 col-md-12">
-                                    <div class="form-group">
-                                        <label for="">Subir foto</label><br>
-                                        <input type="file" name="imagen_mascota" id="" accept="image/*" required>
-                                        @error('imagen_mascota')
-                                            <small class="text-danger">{{ $message }}</small>
-                                        @enderror
-                                     </div> 
-                                </div>
-                                
-
-                                <button type="submit" class="btn btn-primary">Guardar</button>
-                            </div>
-                            </form> 
-                      
+                    <div class="col-xs-12 col-sm-12 col-md-12">
+                        <div class="form-group">
+                            <label for="">Subir foto</label><br>
+                            <input type="file" name="imagen_mascota" id="" accept="image/*" required style="width: 150px;">
+                            @error('imagen_mascota')
+                            <small class="text-danger">{{ $message }}</small>
+                            @enderror
                         </div>
                     </div>
                 </div>
-            </div>
-        </div>
-    </section>
-@endsection
 
+                <div class="actualizar2">
+                    <div class="col-xs-12 col-sm-12 col-md-12">
+                        <div class="form-group">
+                            <label for="nombre_mascota">Nombre de la mascota</label>
+                            <input type="text" name="nombre_mascota" class="form-control" required
+                                value="{{ old('nombre_mascota') }}">
+                        </div>
+                    </div>
+
+                    <div class="col-xs-12 col-sm-12 col-md-12">
+                        <div class="form-group">
+                            <label for="descripcion">Descripcion</label>
+                            <input type="text" name="descripcion" class="form-control" required
+                                value="{{ old('descripcion') }}">
+                        </div>
+                    </div>
+
+                    <div class="col-xs-12 col-sm-12 col-md-12">
+                        <div class="form-group">
+                            <label for="raza">Raza</label>
+                            <select name="raza" id="raza" class="form-control">
+                                @foreach($razas as $raza)
+                                <option value="{{ $raza['raza'] }}">{{ $raza['descripcion'] }}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                    </div>
+
+                    <div class="col-xs-12 col-sm-12 col-md-12">
+                        <div class="form-group">
+                            <label for="color">Color</label>
+                            <select name="color" id="color" class="form-control">
+                                @foreach($colores as $color)
+                                <option value="{{ $color['color'] }}">{{ $color['descripcion'] }}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div>
+
+                    <div class="col-xs-12 col-sm-12 col-md-12">
+                        <div class="form-group">
+                            <label for="tamanio">Tamaño</label>
+                            <select name="tamanio" id="tamanio" class="form-control">
+                                @for ($i = 20; $i <=110; $i=$i + 5) <option value="{{ $i }}">{{ $i }} cm</option>
+                                    @endfor
+                            </select>
+                        </div>
+                    </div>
+
+                    <div class="col-xs-12 col-sm-12 col-md-12">
+                        <div class="form-group">
+                            <label for="especie">Especie</label>
+                            <select name="especie" id="especie" class="form-control">
+                                @foreach($especies as $especie)
+                                <option value="{{ $especie['especie'] }}">{{ $especie['descripcion'] }}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                    </div>
+
+                    <div class="col-xs-12 col-sm-12 col-md-12">
+                        <div class="form-group">
+                            <label for="edad">Edad</label>
+                            <select name="edad" id="edad" class="form-control">
+                                @for ($i = 0; $i <=14; $i++) <option value="{{ $i }}">{{ $i }} años</option>
+                                    @endfor
+                            </select>
+                        </div>
+                    </div>
+
+                </div>
+                <button type="submit" class="btn btn-primary">Guardar</button>
+        </div>
+        </form>
+
+    </div>
+    <a href="#" class="btn-close-modal">X</a>
+
+</div>
--- resources/views/HomeFundacion/Mascotas/index.blade.php
+++ resources/views/HomeFundacion/Mascotas/index.blade.php
@@ -19,7 +19,7 @@
                             </select>
                         </div>
                         <div>
-                            <a class="btn btn-warning" href="{{ route('Mascotas.create')}}">Nuevo</a>
+                            <a class="btn btn-warning" href="#modalsCreate">Nuevo</a>
                         </div>
                     </div>
                 </div>
@@ -35,11 +35,11 @@
                                     <img src="{{ asset("storage/$mascota->imagen_mascota") }}" alt="" />
                                 </div>
                                 <div class="description">
-                                    <h1>{{ $mascota->nombre_mascota }}</h1>
+                                    <h1>{{ $mascota	-> nombre_mascota }}</h1>
                                     <a style="display: none;">{{ $mascota->id }}</a>
                                     <div class="opcionesAdmin">
                                         <form action="{{ route('Mascotas.destroy', $mascota) }}" method="POST">
-                                            <a href="#modals"><img src="{{ asset('img/Home/edit.png') }}" alt="" /></a>
+                                        <a href="#modalsEdit{{ $mascota}}"><img src="{{ asset('img/Home/edit.png') }}" alt="" /></a>
                                             @csrf
                                             @method('DELETE')
                                             <a><button type="submit" style="background:none; border: none;"><img
@@ -50,117 +50,16 @@
                             </div>
                         </div>
 
+
+                        @include('HomeFundacion.Mascotas.editar')
+
+
                         @endforeach
 
-                        <div class="contenedor-modal" id=modals>
-                            <div class="moda">
-                                <div class="informacion">
-                                        <h3 class="page__heading">Nuevo Animal</h3>
-                                    <img src="{{ asset("storage/$mascota->imagen_mascota") }}" alt="Adopta">
-                                    <div class="descripcion">
-                                        <form action="">
-                                            <div class="row">
-                                                <div class="col-xs-12 col-sm-12 col-md-12">
-                                                    <div class="form-group">
-                                                        <label for="nombre_mascota">Nombre de la mascota</label>
-                                                        <input type="text" name="nombre_mascota" class="form-control" required value="{{ old('nombre_mascota', $mascota->nombre_mascota) }}">
-                                                    </div>
-                                                </div>
-
-                                                <div class="col-xs-12 col-sm-12 col-md-12">
-                                                    <div class="form-group">
-                                                        <label for="descripcion">Descripcion</label>
-                                                        <input type="text" name="descripcion" class="form-control" required value="{{ old('descripcion', $mascota->descripcion) }}">
-                                                    </div>
-                                                </div>
-
-                                                <div class="col-xs-12 col-sm-12 col-md-12">
-                                                    <div class="form-group">
-                                                        <label for="raza">Raza</label>
-                                                        <select name="raza" id="raza" class="form-control">
-                                                            @foreach($razas as $key => $raza)
-                                                                <option value="{{ $raza['raza'] }}">{{ $raza['descripcion'] }}</option>
-                                                            @endforeach
-                                                        </select>
-                                                    </div>
-                                                </div>
-
-                                                <div class="col-xs-12 col-sm-12 col-md-12">
-                                                    <div class="form-group">
-                                                        <label for="color">Color</label>
-                                                        <select name="color" id="color" class="form-control">
-                                                            @foreach($colores as $color)
-                                                                <option value="{{ $color['color'] }}">{{ $color['descripcion'] }}</option>
-                                                            @endforeach
-                                                        </select>
-                                                    </div>
-                                                </div>
-
-                                                <div class="col-xs-12 col-sm-12 col-md-12">
-                                                    <div class="form-group">
-                                                        <label for="tamanio">Tamaño</label>
-                                                        <select name="tamanio" id="tamanio" class="form-control">
-                                                            @for ($i = 20; $i <=110; $i = $i + 5)
-                                                            <option value="{{ $i }}">{{ $i }} cm</option>
-                                                            @endfor
-                                                        </select>
-                                                    </div>
-                                                </div>
-
-                                                <div class="col-xs-12 col-sm-12 col-md-12">
-                                                    <div class="form-group">
-                                                        <label for="especie">Especie</label>
-                                                        <select name="especie" id="especie" class="form-control">
-                                                            @foreach($especies as $especie)
-                                                            <option value="{{ $especie['especie'] }}">{{ $especie['descripcion'] }}</option>
-                                                            @endforeach
-                                                        </select>
-                                                    </div>
-                                                </div>
-
-                                                <div class="col-xs-12 col-sm-12 col-md-12">
-                                                    <div class="form-group">
-                                                        <label for="edad">Edad</label>
-                                                        <select name="edad" id="edad" class="form-control">
-                                                            @for ($i = 0; $i <=14; $i++)
-                                                            <option value="{{ $i }}">{{ $i }} años</option>
-                                                            @endfor
-                                                        </select>
-                                                    </div>
-                                                </div>
-
-                                                <div class="col-xs-12 col-sm-12 col-md-12">
-                                                    <div class="form-group">
-                                                        <label for="estado">Estado</label>
-                                                        <select name="estado" id="estado" class="form-control">
-                                                            @foreach($estados as $estado)
-                                                            <option value="{{ $estado['estado'] }}">{{ $estado['descripcion'] }}</option>
-                                                            @endforeach
-                                                        </select>
-                                                    </div>
-                                                </div>
-
-                                                <div class="col-xs-12 col-sm-12 col-md-12">
-                                                    <div class="form-group">
-                                                        <label for="">Subir foto</label><br>
-                                                        <input type="file" name="imagen_mascota" id="" accept="image/*">
-                                                        @error('imagen_mascota')
-                                                            <small class="text-danger">{{ $message }}</small>
-                                                        @enderror
-                                                    </div> 
-                                                </div>
-                                                <button type="submit" class="btn btn-primary">Guardar</button>
-                                            </div>
-                                        </form>
-                                    </div>
-                                    {{-- <button class="transicion2" type="submit"><span>Solicitar Adopcion</span></button> --}}
-                                </div>
-                                {{-- <a href="#" class="btn-close-modal">X</a> --}}
-                            </div>
+                        @include('HomeFundacion.Mascotas.crear')
                     </div>
                 </div>
             </div>
         </div>
-    </div>
 </section>
 @endsection
