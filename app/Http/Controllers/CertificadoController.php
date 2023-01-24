92bf7839a50c98c36bd44021aa38fa0f9e1ad64b
JCGarcia25 <jecg2509@gmail.com>
2023-01-24 05:29:29 -0500

    limpieza de código para el apartado de certificados

--- app/Http/Controllers/CertificadoController.php
+++ app/Http/Controllers/CertificadoController.php
@@ -6,7 +6,6 @@ use Carbon\Carbon;
 use App\Models\Certificado;
 use App\Models\Fundacion;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Auth;
 use PhpOffice\PhpWord\TemplateProcessor;
@@ -96,60 +95,6 @@ class CertificadoController extends Controller
             return [false, $e];
         }
 
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    /*public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    /*public function edit(Campania $Campania)
-    {
-        return view('HomeFundacion.Campanias.editar', compact('Campania'));
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    /*public function update(Request $request, Campania $Campania)
-    {
-        //Validaciendo de los campos enviados desde la vista crear
-        request()->validate([
-            'nombre_campania' => 'required',
-            'descripcion' => 'required',
-            'meta_donaciones' => 'required|numeric',
-            'actual_donado' => 'required|numeric',
-        ]);
-        //Recibiendo un archivo de tipo imagen y tranformadolo en una ruta para así guardarlo en la base de datos
-        if ($request->has('imagen_campania')){
-            $profileImage = time() . '.' . $request->file('imagen_campania')->getClientOriginalExtension();
-            Storage::disk('public')->put($profileImage,file_get_contents($request->file('imagen_campania')->getPathName()) );
-            $Campania->imagen_campania = $profileImage;
-        }
-        //operador ternario, siempre preguntará si le llega algo de ese dato, de ser así reemplacelo, sino, deje el anterior
-        $Campania->nombre_campania = $request->has('nombre_campania') ? $request->nombre_campania : $Campania->nombre_serviciio;
-        $Campania->descripcion = $request->has('descripcion') ? $request->descripcion : $Campania->descripcion;
-        $Campania->meta_donaciones = $request->has('meta_donaciones') ? $request->meta_donaciones : $Campania->meta_donaciones;
-        $Campania->actual_donado = $request->has('actual_donado') ? $request->actual_donado : $Campania->actual_donado;
-        $Campania->save();
-
-        return redirect()->route('Campanias.index');
     }
 
     /**
